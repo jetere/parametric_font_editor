13a3aeaf03d61024fcf56aabe6db40d4cb928991
Jeti <jatinkunjam17@gmail.com>
2026-08-06 05:36:03 +0545

    feat(ui/export): update app theme

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1150.0, 720.0])
-            .with_title("15-Box Parametric Font Studio (Rust Native)"),
+            .with_title("paramatic_font_editor"),
         ..Default::default()
     };
 
